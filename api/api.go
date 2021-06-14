a3b7f7fec6067ab5d180980990e27c0bf59695a6
Anton Shangareev <shananton2000@gmail.com>
2021-06-14 23:47:02 +0300

    Add logging

--- api/api.go
+++ api/api.go
@@ -7,6 +7,7 @@ import (
 	"github.com/mp-hl-2021/splinter/monitoring"
 	"github.com/mp-hl-2021/splinter/usecases"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
 	"net/http"
 )
 
@@ -18,8 +19,28 @@ func NewApi(u usecases.UserInterface, a auth.Authenticator) *Api {
 	return &Api{v1: v1.NewApi(u, a)}
 }
 
+type responseWriterObserver struct {
+	http.ResponseWriter
+	status int
+}
+
+func (o *responseWriterObserver) WriteHeader(code int) {
+	o.ResponseWriter.WriteHeader(code)
+	o.status = code
+}
+
+func logRequestMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+		ow := &responseWriterObserver{w, http.StatusOK}
+		next.ServeHTTP(ow, r)
+		log.Printf("method = %v, url = %v, status = %v", r.Method, r.URL.Path, ow.status)
+	})
+}
+
 func (a *Api) Router() http.Handler {
 	router := mux.NewRouter()
+
+	router.Use(logRequestMiddleware)
 
 	a.v1.Router(router.PathPrefix("/api/v1").Subrouter())
 	router.Handle("/metrics", promhttp.Handler())
